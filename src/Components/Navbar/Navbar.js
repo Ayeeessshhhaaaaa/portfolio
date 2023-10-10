cb25efc02084730d113cb90d4d26423cf8a9a9b2
Ayeeessshhhaaaaa <ayeshafaz90@gmail.com>
2023-10-11 00:54:05 +0530

    x

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -3,19 +3,26 @@ import './Navbar.css';
 import { Link, useNavigate, useLocation } from 'react-router-dom';
 
 function Navbar() {
+  const navigate = useNavigate();
+  const location = useLocation();
 
+  // Handle navigation to the homepage ("/")
   return (
-<nav className='mt-5'>
-  <ul>
-    <li><Link to="/portfolio#About" className="nav-link">About</Link></li>
-    <li><Link to="/portfolio#Services" className="nav-link">Services</Link></li>
-    <li><Link to="/portfolio#Projects" className="nav-link">Portfolio</Link></li>
-    <li><Link to="/portfolio#Skills" className="nav-link">Skills</Link></li>
-    <li><Link to="/portfolio#Contact" className="nav-link">Contact</Link></li>
-    <li><Link to="/education" className="nav-link">Education</Link></li>
-  </ul>
-</nav>
+    <nav className='mt-5'>
+      <ul>
+        {location.pathname === '/' ? null : (
+        <li><Link to="/">Home</Link></li>
+        )}
+        <li><a href="#About">About</a></li>
+        <li><a href="#Services">Services</a></li>
+        <li><a href="#Projects">Portfolio</a></li>
+        <li><a href="#Skills">Skills</a></li>
+        <li><a href="#Contact">Contact</a></li>
+        <li><Link to="/education">Education</Link></li>
+      </ul>
+    </nav>
   );
 }
 
 export default Navbar;
+
