38250f3391b926249ada6c4b3d92b16ae8c1041d
Ayeeessshhhaaaaa <ayeshafaz90@gmail.com>
2023-10-10 23:46:18 +0530

    x

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -7,11 +7,11 @@ function Navbar() {
   return (
     <nav className='mt-5'>
       <ul>
-            <li><a href="#About">About</a></li>
-            <li><a href="#Services">Services</a></li>
-            <li><a href="#Projects">Portfolio</a></li>
-            <li><a href="#Skills">Skills</a></li>
-         <li><a href="#Contact">Contact</a></li>
+            <li><a href="/#About">About</a></li>
+            <li><a href="/#Services">Services</a></li>
+            <li><a href="/#Projects">Portfolio</a></li>
+            <li><a href="/#Skills">Skills</a></li>
+         <li><a href="/#Contact">Contact</a></li>
         <li><Link to="/education">Education</Link></li>
       </ul>
     </nav>
