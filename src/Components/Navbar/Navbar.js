0d015e5ced57a297eb3ba4e77b2ae22bdf3a8a4c
Ayeeessshhhaaaaa <ayeshafaz90@gmail.com>
2023-10-11 01:01:50 +0530

    vvv

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -13,11 +13,11 @@ function Navbar() {
         {location.pathname === '/' ? null : (
         <li><Link to="/">Home</Link></li>
         )}
-        <li><a href="#About">About</a></li>
-        <li><a href="#Services">Services</a></li>
-        <li><a href="#Projects">Portfolio</a></li>
-        <li><a href="#Skills">Skills</a></li>
-        <li><a href="#Contact">Contact</a></li>
+        <li><a href="/#About">About</a></li>
+        <li><a href="/#Services">Services</a></li>
+        <li><a href="/#Projects">Portfolio</a></li>
+        <li><a href="/#Skills">Skills</a></li>
+        <li><a href="/#Contact">Contact</a></li>
         <li><Link to="/education">Education</Link></li>
       </ul>
     </nav>
