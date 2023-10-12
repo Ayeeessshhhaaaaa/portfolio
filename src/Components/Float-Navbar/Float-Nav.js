5824320f5068352e0fba4e8a4465f6fecde67699
Ayeeessshhhaaaaa <ayeshafaz90@gmail.com>
2023-10-13 00:02:39 +0530

    final

--- src/Components/Float-Navbar/Float-Nav.js
+++ src/Components/Float-Navbar/Float-Nav.js
@@ -4,21 +4,35 @@ import {AiOutlineHome} from 'react-icons/ai'
 import { MdOutlineDesignServices } from 'react-icons/md'
 import { BiMessageSquareDetail } from 'react-icons/bi'
 import { BiBookAlt} from 'react-icons/bi';
-import { RiServiceLine } from 'react-icons/ri'
-import { AiFillFolderOpen } from 'react-icons/ai'
+import { RiServiceLine } from 'react-icons/ri';
+import { AiFillFolderOpen } from 'react-icons/ai';
 import { useState } from 'react'
-
+import { useLocation } from 'react-router-dom';
 
 const FloatNav = () => {
   const [activeNav, setActiveNav] = useState('#');
+  const location = useLocation();
+
   return (
   <nav className='nav'>
+  {location.pathname === '/education' ? (
+          <>
+              <a href='/portfolio' onClick={() => setActiveNav('/portfolio')} className={activeNav === '/portfolio' ? 'active' : ''}><AiOutlineHome/></a>
+              <a href='/education' onClick={() => setActiveNav('/education')} className={activeNav === '/education' ? 'active' : ''}><BiBookAlt/></a>
+          </>
+  ):(
+    <>
+    {location.pathname !== '/portfolio' && (
+ <a href='/portfolio' onClick={() => setActiveNav('/portfolio')} className={activeNav === '/portfolio' ? 'active' : ''}><AiOutlineHome/></a>
+    )}
     <a href='#About' onClick={() => setActiveNav('#About')} className={activeNav === '#About' ? 'active' : ''}><AiOutlineHome/></a>
     <a href='#Services' onClick={() => setActiveNav('#Services')} className={activeNav === '#Services' ? 'active' : ''}><RiServiceLine/></a>
     <a href='#Skills' onClick={() => setActiveNav('#Skills')} className={activeNav === '#Skills' ? 'active' : ''}><MdOutlineDesignServices/></a>
     <a href='#Projects' onClick={() => setActiveNav('#Projects')} className={activeNav === '#Projects' ? 'active' : ''}><AiFillFolderOpen/></a>
     <a href='/education' onClick={() => setActiveNav('/education')} className={activeNav === '/education' ? 'active' : ''}><BiBookAlt/></a>
     <a href='#Contact' onClick={() => setActiveNav('#Contact')} className={activeNav === '#Contact' ? 'active' : ''}><BiMessageSquareDetail/></a>
+    </>
+  )}
   </nav>
   )
 }
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,27 +1,35 @@
 import React from 'react';
 import './Navbar.css';
-import { Link, useNavigate, useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 function Navbar() {
-  const navigate = useNavigate();
   const location = useLocation();
 
   return (
     <nav className='mt-5'>
       <ul>
-        {location.pathname === '/' ? null : (
-        <li><Link to="/portfolio">Home</Link></li>
+        {location.pathname === '/education' ? (
+          <>
+          <li><Link to="/portfolio">Home</Link></li>
+          <li><Link to="/education">Education</Link></li>
+          </>
+        
+        ) : (
+          <>
+            {location.pathname !== '/portfolio' && (
+              <li><Link to="/portfolio">Home</Link></li>
+            )}
+            <li><a href="#About">About</a></li>
+            <li><a href="#Services">Services</a></li>
+            <li><a href="#Skills">Skills</a></li>
+            <li><a href="#Projects">Portfolio</a></li>
+            <li><a href="#Contact">Contact</a></li>
+            <li><Link to="/education">Education</Link></li>
+          </>
         )}
-        <li><a href="#About">About</a></li>
-        <li><a href="#Services">Services</a></li>
-        <li><a href="#Skills">Skills</a></li>
-        <li><a href="#Projects">Portfolio</a></li>
-        <li><a href="#Contact">Contact</a></li>
-        <li><Link to="/education">Education</Link></li>
       </ul>
     </nav>
   );
 }
 
 export default Navbar;
-
